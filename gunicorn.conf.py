d119932f663a830ff576f34f184dcf8407b459ca
XAMadmin <50042444+XAMadmin@users.noreply.github.com>
2022-03-03 17:05:21 +0800

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -13,3 +13,16 @@ bind = "0.0.0.0:8080"
 # 启动的进程数
 workers = multiprocessing.cpu_count()
 worker_class = 'gevent'
+
+
+# ========或者一下配置===============================================
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 1
+accesslog ="-" # STDOUT
+access_log_format = '%(h)s %(l)s %(u)s %(t)s"%(r)s" %(s)s %(b)s"%(q)s""%(D)s"'
+bind ="0.0.0.0:5000"
+keepalive = 120
+timeout = 120
+worker_class ="gthread"
+threads = 3
